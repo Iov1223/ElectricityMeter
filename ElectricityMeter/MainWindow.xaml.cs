99807c79caafe43615f840768f4a2ddb8ceb10be
Andrey Popov <andrej_1987@bk.ru>
2023-01-17 15:51:29 +0500

    Куча вопросов

--- ElectricityMeter/MainWindow.xaml.cs
+++ ElectricityMeter/MainWindow.xaml.cs
@@ -20,16 +20,33 @@ namespace ElectricityMeter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string[] monthArr;
+        private Label labelMonth;
+        private Label labelNight;
+        private Label labelDay;
+        private Label labelSum;
+        private Label labelMonthName;
+        private Button buttonResult;
+        private TextBlock textBlockTotal;
+        private Button buttonSum;
+        private TextBox textBoxInputData;
         public MainWindow()
         {
             InitializeComponent();
             ElectricityMeter.Rows = 14;
             ElectricityMeter.Columns = 4;
+            monthArr = new string[]
+            {
+                "Январь", "Февраль", "Март",
+                "Апрель", "Май", "Июнь",
+                "Июль", "Август", "Сентябрь",
+                "Октябрь", "Ноябрь", "Декабрь"
+            };
         }
 
         private void ElectricityMeter_Loaded(object sender, RoutedEventArgs e)
         {
-            int num = 1;
+            int num = 0;
 
             for (int i = 0; i < ElectricityMeter.Rows; i++)
             {
@@ -37,92 +54,119 @@ namespace ElectricityMeter
                 {
                     if (j == 0 && i == 0)
                     {
-                        TextBlock textBlockMonth = new TextBlock();
-                        textBlockMonth.Text = "Месяц";
-                        textBlockMonth.FontSize = 20;
-                        textBlockMonth.FontWeight = FontWeights.Bold;
-                        textBlockMonth.Padding = new Thickness(8);
-                        textBlockMonth.TextAlignment = TextAlignment.Center;
-                        ElectricityMeter.Children.Add(textBlockMonth);
+                        labelMonth = new Label();
+                        labelMonth.Content = "Месяц";
+                        labelMonth.FontSize = 17;
+                        labelMonth.FontWeight = FontWeights.Bold;
+                        labelMonth.Padding = new Thickness(8);
+                        labelMonth.BorderBrush = Brushes.Black;
+                        labelMonth.BorderThickness = new Thickness(0.5);
+                        labelMonth.HorizontalContentAlignment = HorizontalAlignment.Center;
+                        ElectricityMeter.Children.Add(labelMonth);
                     }
                     else if (i == 0 && j == 1)
                     {
-                        TextBlock textBlockNight = new TextBlock();
-                        textBlockNight.FontSize = 20;
-                        textBlockNight.FontWeight = FontWeights.Bold;
-                        textBlockNight.Padding = new Thickness(8);
-                        textBlockNight.Text = "Ночь";
-                        textBlockNight.TextAlignment = TextAlignment.Center;
-                        ElectricityMeter.Children.Add(textBlockNight);
+                        labelNight = new Label();
+                        labelNight.FontSize = 17;
+                        labelNight.FontWeight = FontWeights.Bold;
+                        labelNight.Padding = new Thickness(8);
+                        labelNight.Content = "Ночь (Кол-во в кВт.ч)";
+                        labelNight.BorderBrush = Brushes.Black;
+                        labelNight.BorderThickness = new Thickness(0.5);
+                        labelNight.HorizontalContentAlignment = HorizontalAlignment.Center;
+                        ElectricityMeter.Children.Add(labelNight);
                         
                     }
                     else if (i == 0 && j == 2)
                     {
-                        TextBlock textBlockDay = new TextBlock();
-                        textBlockDay.FontSize = 20;
-                        textBlockDay.FontWeight = FontWeights.Bold;
-                        textBlockDay.Padding = new Thickness(8);
-                        textBlockDay.Text = "День";
-                        textBlockDay.TextAlignment = TextAlignment.Center;
-                        ElectricityMeter.Children.Add(textBlockDay);
+                        labelDay = new Label();
+                        labelDay.FontSize = 17;
+                        labelDay.FontWeight = FontWeights.Bold;
+                        labelDay.Padding = new Thickness(8);
+                        labelDay.Content = "День (Кол-во в кВт.ч)";
+                        labelDay.BorderBrush = Brushes.Black;
+                        labelDay.BorderThickness = new Thickness(0.5);
+                        labelDay.HorizontalContentAlignment = HorizontalAlignment.Center;
+                        ElectricityMeter.Children.Add(labelDay);
                         
                     }
                     else if (i == 0 && j == 3)
                     {
-                        TextBlock textBlockSum = new TextBlock();
-                        textBlockSum.FontSize = 20;
-                        textBlockSum.FontWeight = FontWeights.Bold;
-                        textBlockSum.Padding = new Thickness(8);
-                        textBlockSum.Text = "Сумма";
-                        textBlockSum.TextAlignment = TextAlignment.Center;
-                        ElectricityMeter.Children.Add(textBlockSum);
-                        
+                        labelSum = new Label();
+                        labelSum.FontSize = 17;
+                        labelSum.FontWeight = FontWeights.Bold;
+                        labelSum.Padding = new Thickness(8);
+                        labelSum.Content = "Сумма в рублях";
+                        labelSum.BorderBrush = Brushes.Black;
+                        labelSum.BorderThickness = new Thickness(0.5);
+                        labelSum.HorizontalContentAlignment = HorizontalAlignment.Center;
+                        ElectricityMeter.Children.Add(labelSum);
                     }
                     else if (j == 0 && i != 0 && i != 13)
                     {
-                        TextBlock textBlocNumMonth = new TextBlock();
-                        textBlocNumMonth.FontSize = 20;
-                        textBlocNumMonth.FontWeight = FontWeights.Bold;
-                        textBlocNumMonth.Padding = new Thickness(8);
-                        textBlocNumMonth.Text = num.ToString();
-                        textBlocNumMonth.TextAlignment = TextAlignment.Center;
-                        ElectricityMeter.Children.Add(textBlocNumMonth);
+                        labelMonthName = new Label();
+                        labelMonthName.FontSize = 17;
+                        labelMonthName.FontWeight = FontWeights.Bold;
+                        labelMonthName.Padding = new Thickness(8);
+                        labelMonthName.Content = monthArr[num];
+                        labelMonthName.BorderBrush = Brushes.Black;
+                        labelMonthName.BorderThickness = new Thickness(0.5);
+                        labelMonthName.HorizontalContentAlignment = HorizontalAlignment.Center;
+                        ElectricityMeter.Children.Add(labelMonthName);
                         num++;
                     }
                     else if (j == 3 && i != 0)
                     {
-                        TextBlock tb = new TextBlock();
-                        ElectricityMeter.Children.Add(tb);
+                        buttonResult = new Button();
+                        buttonResult.BorderBrush = Brushes.Black;
+                        buttonResult.BorderThickness = new Thickness(0.5);
+                        buttonResult.FontSize = 17;
+                        buttonResult.FontWeight = FontWeights.Bold;
+                        buttonResult.Padding = new Thickness(8);
+                        buttonResult.Content = "Посчитать";
+                        buttonResult.HorizontalContentAlignment = HorizontalAlignment.Center;
+                        ElectricityMeter.Children.Add(buttonResult);
                     }
                     else if (i == 13 && j== 0)
                     {
-                        TextBlock textBlockTotal = new TextBlock();
-                        textBlockTotal.FontSize = 20;
-                        textBlockTotal.FontWeight = FontWeights.Bold;
-                        textBlockTotal.Padding = new Thickness(8);
+                        textBlockTotal = new TextBlock();
                         textBlockTotal.Text = "";
-                        textBlockTotal.TextAlignment = TextAlignment.Center;
                         ElectricityMeter.Children.Add(textBlockTotal);
                     }
                     else if (i == 13 && j == 1)
                     {
-                        Button buttonSum = new Button();
-                        buttonSum.Content = "Посчитать";
+                        buttonSum = new Button();
+                        buttonSum.FontSize = 17;
+                        buttonSum.FontWeight = FontWeights.Bold;
+                        buttonSum.Content = "Итого";
                         buttonSum.BorderBrush = Brushes.Black;
+                        buttonSum.Background = Brushes.DodgerBlue;
+                        buttonSum.Foreground = Brushes.White;  
                         ElectricityMeter.Children.Add(buttonSum);
                     }
                     else 
                     {
-                        TextBox textBoxInputData = new TextBox();
+                        textBoxInputData = new TextBox();
+                        textBoxInputData.Text = "2";
                         textBoxInputData.Padding = new Thickness(8);
                         textBoxInputData.TextAlignment = TextAlignment.Center;
-                        textBoxInputData.FontSize = 20;
+                        textBoxInputData.FontSize = 17;
                         textBoxInputData.FontWeight = FontWeights.Bold;
                         textBoxInputData.BorderBrush = Brushes.Black;
                         ElectricityMeter.Children.Add(textBoxInputData);
-
                     }
                 }
+            }
+        }
+        private void buttonResult_Clic(object sender, RoutedEventArgs e)
+        {
+            if (textBoxInputData.Text != null)
+            {
+                buttonResult.Content = Convert.ToInt32(textBoxInputData.Text) + Convert.ToInt32(textBoxInputData.Text);
+            }
+            else
+            {
+                MessageBox.Show("Ничего нет");
             }
         }
     }
